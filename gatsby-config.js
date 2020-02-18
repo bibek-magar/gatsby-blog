25d8f67a6621d93f88e6861d0acad19c5a4778fc
beevekmgr <beevekmgr@gmail.com>
2020-02-18 21:36:39 +0545

    Adding all new blog code for gatsby:

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
