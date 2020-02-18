e27a2a4f922ec2bd1a95e49f7ac7e459453036b2
beevekmgr <beevekmgr@gmail.com>
2020-02-18 21:43:07 +0545

    Added total count post

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default ({ data }) => (
     <SEO title="Home" />
     <div>
       <h1>Bibek's blog</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <BlogLink>
